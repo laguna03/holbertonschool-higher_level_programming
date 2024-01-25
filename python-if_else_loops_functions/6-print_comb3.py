b90a7270b7fd326305747819195956124fc80bd8
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-01-25 17:00:31 -0400

    Print comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for number in range(0, 99):
-    if number / 10 < number % 10:
-        print("{:02d}, ".format(number), end="")
-    if number == 79:
-        print("89")
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
+else:
+    print("{}{}".format(i, j), end=", ")
