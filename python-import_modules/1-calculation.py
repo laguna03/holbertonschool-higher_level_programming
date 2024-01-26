8ed4ae67850d3262158776e4153fb4ae847883ed
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-01-26 17:39:34 -0400

    Calculator

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""Python script that imports functions from the file calculator_1.py,
-    does some Maths, and prints the result.
-"""
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
