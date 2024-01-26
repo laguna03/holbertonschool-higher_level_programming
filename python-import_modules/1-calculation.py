c31d03f72cd67d1105247f7c7205e54a6f244f58
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-01-26 17:38:48 -0400

    Calculator

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,8 +2,8 @@
 """Python script that imports functions from the file calculator_1.py,
     does some Maths, and prints the result.
 """
-from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
