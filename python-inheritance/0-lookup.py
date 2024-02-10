9182ce1eca9722ec8008ef6f395e57f4bea9c7e2
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-02-10 17:48:48 -0400

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Task 0 - Function which return the list of all available
+attributes and methods of an object
+"""
+
+
 def lookup(obj):
-    """Returns list of available attributes and methods of  obj"""
+    """
+    Lookup receibe obj and return all atributes and methods of obj
+    """
+
     return dir(obj)
