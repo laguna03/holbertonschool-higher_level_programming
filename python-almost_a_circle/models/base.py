5c69879197010dece9d1b99cbc41009a6185733b
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-02-12 15:09:59 -0400

    Rearrange files

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Base:
+    """ Base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialize the Base class """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
