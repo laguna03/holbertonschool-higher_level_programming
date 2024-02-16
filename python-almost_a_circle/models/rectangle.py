c8e07274af8c3f6da78aeb9d42fd1602d54642fe
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-02-16 13:35:36 -0400

    Rectangle debugging

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initialize the Rectangle class """
-
+        super().__init__()
         self.__width = width
         self.__height = height
         self.__x = x
