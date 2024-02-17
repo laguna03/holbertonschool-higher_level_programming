e73b061b50614169beaa2f0b4e8671ce73e6a775
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-02-17 12:47:11 -0400

    adding json strings dict

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,3 +14,10 @@ class Base:
             """ Increment the number of objects"""
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+def to_json_string(list_dictionaries):
+    """ Returns the JSON string representation of list_dictionaries """
+    import json
+    if list_dictionaries is None or list_dictionaries == []:
+        return "[]"
+    return json.dumps(list_dictionaries)
