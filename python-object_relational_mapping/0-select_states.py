faaf5a8479fe42c8ba63c60a0d1676c37246aa4a
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-19 11:48:12 -0400

    First commit

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+'''
+Select all lists form db hbtn_0e_0usa
+'''
+import MySQLdb
+import sys
+
+class DataBase:
+    '''
+    class representimg conexion to MySQL db
+    '''
+def __init__(self, arg_user, arg_password, arg_database):
+    self.user = arg_user
+    self.password = arg_password
+    self.database= arg_database
+    db = MySQLdb.Connect(
+        host='localhost',
+        port=3306,
+        user=self.user,
+        password=self.password,
+        db=self.database
+    )
+    self.cursor = db.cursor()
+
+    def print_all_states(self):
+        self.cur.execute("SELECT * FROM state")
+        all_states = self.cur.fetchall()
+        for states in all_states:
+            print(f"({states[0]}, '{states[1]}')")
+
+            if __name__ == "__main__":
+                args = sys.argvarg_user = args[1]
+                arg_password = args[2]
+                arg_database = args[3]
+                all_states = DataBase(arg_user, arg_password, arg_database)
+                all_states.print_all_states()
