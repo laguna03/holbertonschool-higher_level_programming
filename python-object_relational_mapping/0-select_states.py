4f353c0b4c70f0199ac154939ec9197026f25aa1
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-19 11:55:27 -0400

    recommiting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
-"""print all states from a database"""
+'''
+Select all lists form db hbtn_0e_0usa
+'''
+import MySQLdb
+import sys
 
+class DataBase:
+    '''
+    class representimg conexion to MySQL db
+    '''
+def __init__(self, arg_user, arg_password, arg_database):
+    self.user = arg_user
+    self.password = arg_password
+    self.database= arg_database
+    db = MySQLdb.Connect(
+        host='localhost',
+        port=3306,
+        user=self.user,
+        password=self.password,
+        db=self.database
+    )
+    self.cursor = db.cursor()
 
-import MySQLdb as sql
-from sys import argv
+    def print_all_states(self):
+        self.cur.execute("SELECT * FROM state")
+        all_states = self.cur.fetchall()
+        for states in all_states:
+            print(f"({states[0]}, '{states[1]}')")
 
-if __name__ == '__main__':
-    db = sql.connect(host="localhost",
-                     port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+            if __name__ == "__main__":
+                args = sys.argvarg_user = args[1]
+                arg_password = args[2]
+                arg_database = args[3]
+                all_states = DataBase(arg_user, arg_password, arg_database)
+                all_states.print_all_states()
