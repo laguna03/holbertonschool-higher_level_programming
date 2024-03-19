d5895b02775bc2f20570745fff02eaf59223ae29
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-19 14:14:13 -0400

    Filter by argument state_name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,9 +5,11 @@ import MySQLdb as sql
 from sys import argv
 
 if __name__ == '__main__':
-    db = sql.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = sql.connect(host="localhost",
+                      port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}" .format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE\
+                BINARY name = '{}".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
