09be85a992436a5a6f83fc062f993a444fd879a7
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-19 13:44:09 -0400

    fixing sql syntax

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ from sys import argv
 if __name__ == '__main__':
     db = sql.connect(host="localhost", port=3306, user_name=argv[1], password=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
