351a0ef94b0b78e448e85d1cff950b5e8de7bdd8
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-19 12:05:52 -0400

    recommiting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,40 @@
-'''
-Select all lists form db hbtn_0e_0usa
+#!/usr/bin/python3
+
+''''
+Select all lists from the database hbtn_0e_0usa
 '''
 import MySQLdb
 import sys
 
+
 class DataBase:
     '''
-    class representimg conexion to MySQL db
+    Class representing connection to MySQL database
     '''
-def __init__(self, arg_user, arg_password, arg_database):
-    self.user = arg_user
-    self.password = arg_password
-    self.database= arg_database
-    db = MySQLdb.Connect(
-        host='localhost',
-        port=3306,
-        user=self.user,
-        password=self.password,
-        db=self.database
-    )
-    self.cursor = db.cursor()
+    def __init__(self, arg_user, arg_password, arg_database):
+        self.user = arg_user
+        self.password = arg_password
+        self.database = arg_database
+        db = MySQLdb.Connect(
+                            host='localhost',
+                            port=3306,
+                            user=self.user,
+                            password=self.password,
+                            db=self.database
+                            )
+        self.cur = db.cursor()
 
     def print_all_states(self):
-        self.cur.execute("SELECT * FROM state")
+        self.cur.execute("SELECT * FROM states")
         all_states = self.cur.fetchall()
         for states in all_states:
             print(f"({states[0]}, '{states[1]}')")
 
-            if __name__ == "__main__":
-                args = sys.argvarg_user = args[1]
-                arg_password = args[2]
-                arg_database = args[3]
-                all_states = DataBase(arg_user, arg_password, arg_database)
-                all_states.print_all_states()
+
+if __name__ == "__main__":
+    args = sys.argv
+    arg_user = args[1]
+    arg_password = args[2]
+    arg_database = args[3]
+    all_states = DataBase(arg_user, arg_password, arg_database)
+    all_states.print_all_states()
