24a80d34dab4c469403d3c8c955367923c4f966d
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-22 19:38:41 -0400

    filter cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-''''
-Select all lists from the database hbtn_0e_0usa
-'''
-import MySQLdb
-import sys
-from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.Connect(
-                    host='localhost',
-                    port=3306,
-                    user=argv[1],
-                    password=argv[2],
-                    db=argv[3]
-                    )
-    cursor = db.cursor()
-    cursor.execute(
-        """SELECT cities.name FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name LIKE %s""", (argv[4], )
-        )
-    print(', '.join(["{:s}".format(row[0])for row in cursor.fetchall()]))
+"""
+This script lists all cities of a state given as parameter
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    username, password, database_name, state_name = sys.argv[1:]
+
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=database_name)
+    cur = db.cursor()
+
+    cur.execute("""
+        SELECT cities.name
+        FROM cities
+        LEFT JOIN states
+        ON cities.state_id = states.id
+        WHERE states.name LIKE BINARY %s
+        ORDER BY cities.id
+    """, (state_name,))
+
+    rows = cur.fetchall()
+    data = [elem for row in rows for elem in row]
+
+    print(*data, sep=', ')
